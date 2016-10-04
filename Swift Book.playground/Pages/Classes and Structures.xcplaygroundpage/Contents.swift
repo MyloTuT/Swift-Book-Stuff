b35e9b31ccb1dc9c34aec3e261a60aafcf6b083b
Myles Rayfield <mrayfield@renttherunway.com>
2016-10-04 15:13:30 -0400

    creating a branch from development

--- Swift Book.playground/Pages/Classes and Structures.xcplaygroundpage/Contents.swift
+++ Swift Book.playground/Pages/Classes and Structures.xcplaygroundpage/Contents.swift
@@ -1,4 +1,6 @@
-//:
-//:
-//Classes and Structures
+//:Classes and Structures
+// Classes and structures are general-purpose, flexible constructs that become the building blocks of your program's code. You define properties and methods to add functionality to your classes and structures by using exactly the same syntax as for constants, var, and functions.
+
+
+
 
